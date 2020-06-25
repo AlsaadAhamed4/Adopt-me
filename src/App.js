3d464cf79e93eaa157bea809100bb822284d28f5
Alsaad Ahamed <alsaadahamed4@gmail.com>
2020-06-25 20:50:57 +0530

    Parcel build component - extract out pet component
    - Parcel for bundling our files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
@@ -23,6 +19,11 @@ const App = () => {
       name: "Doink",
       animal: "Cat",
       breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      name: "Visky",
+      animal: "Dog",
+      breed: "German Shefford",
     }),
   ]);
 };
